3e567a8f956f9e4222f656c70391f5a5092514c7
kishorchouhan <devkishorbwr@gmail.com>
2019-07-06 02:23:14 +0530

    Updates

---
+++ precache-manifest.d06649fdce9c267bd14efd1e788b2c8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f50741502a71be275836458034df001",
+    "url": "/Todo-App-Using-ReactJS/index.html"
+  },
+  {
+    "revision": "f4929bded621ee9fd6fe",
+    "url": "/Todo-App-Using-ReactJS/static/css/main.748796e8.chunk.css"
+  },
+  {
+    "revision": "34964efd9cb9aa3f9b9a",
+    "url": "/Todo-App-Using-ReactJS/static/js/2.34c9b1b3.chunk.js"
+  },
+  {
+    "revision": "f4929bded621ee9fd6fe",
+    "url": "/Todo-App-Using-ReactJS/static/js/main.f7318d16.chunk.js"
+  },
+  {
+    "revision": "11d0ff5b425c865c3682",
+    "url": "/Todo-App-Using-ReactJS/static/js/runtime~main.ea5cdfb0.js"
+  }
+]);
